00762adc273b669ba84936de6339660978e1450d
ShantanuR03 <110837041+ShantanuR03@users.noreply.github.com>
2024-09-27 11:33:45 +0530

    Update s3config.js

--- s3config.js
+++ s3config.js
@@ -1,7 +1,7 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 
 const s3Client = new S3Client({
-    region: 'eu-north-1',
+    region: 'us-east-1',
     credentials: {
       accessKeyId: import.meta.env.VITE_AWS_ACCESS_KEY_ID,
       secretAccessKey: import.meta.env.VITE_AWS_SECRET_ACCESS_KEY,
